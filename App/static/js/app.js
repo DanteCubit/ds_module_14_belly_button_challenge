e0d33e0492fde657496fdec01a06d8135bb47ee4
DonteCubit <madden.steven.j@gmail.com>
2024-09-07 13:54:01 -0400

    fixing_js

--- App/static/js/app.js
+++ App/static/js/app.js
@@ -125,7 +125,7 @@ function init() {
 
     // Build charts and metadata panel with the first sample
     buildCharts(first_sample);
-    buildMetadata(first); //change for website
+    buildMetadata(first_sample); //change for website
   });
 }
 
